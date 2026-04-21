8a045002298e66a47157c65a6b5eef8ebc3ca545
OscarsGlobal <chrisj.price@canterbury.ac.nz>
2026-04-21 21:21:26 +1200

    Update example comments in print.oscars.R
    Added the extra second argument n = 2 giving number of parameters

--- R/print.oscars.R
+++ R/print.oscars.R
@@ -12,14 +12,14 @@
 #' @seealso \code{\link{oscars}}
 #'
 #' @examples
-#' # Branins camel function with global minima of f = -1.0316 at
-#' # (0.0898,0.7127) and (0.0898,-0.7127) with four other local minima
+#' # Branins camel function with global minimum of f = -1.0316 at
+#' # (0.0898,0.7127) and (0.0898,-0.7127) with four other local minimizers
 #' camel <- function(par) {
 #'   x = par[1]
 #'   y = par[2]
 #'   f = 4*x^2 - 2.1*x^4 + (1/3)*x^6 + x*y + 4*(y^4-y^2)
 #'   return(f) }
-#' out <- oscars(camel, lwr = c(-5,-5), upr = c(5,5))
+#' out <- oscars(camel, n = 2, lwr = c(-5,-5), upr = c(5,5))
 #' out
 #'
 #' @export
